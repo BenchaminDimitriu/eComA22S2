79d39d2ddcc8fe945653e0ad4f43e9567d183d1e
Jonathan <DimitriuJonathan@outlook.com>
2022-08-25 12:44:29 -0400

    added a bootstrapped application

---
+++ app/core/App.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\core;
+
+/* Rounting all request to the appropriate controller method
+e.g
+localhost/person/add -> run the add method in the personController class
+localhost/person/delete -> run the delete method in the personController class
+*/
+
+
+
+
+
+
+
+class App{
+	public function __construct(){
+		echo $_GET['url'];
+		//place the routing algorithm here
+	}
+}
---
+++ app/core/init.php
@@ -0,0 +1,3 @@
+<?php
+//echo is useful for debugging
+require_once('app\core\App.php');
