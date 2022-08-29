b3dcd895c9b8f3becbe88aad779b9aa4e170816b
DeadBwain <DimitriuJonathan@outlook.com>
2022-08-29 11:50:07 -0400

    Routing algorithm
    autoloader included

--- app/core/App.php
+++ app/core/App.php
@@ -14,8 +14,62 @@ localhost/person/delete -> run the delete method in the personController class
 
 
 class App{
+
+	private $controller = 'Main';
+	private $method = 'index';  //default name of index
+
 	public function __construct(){
 		echo $_GET['url'];
 		//place the routing algorithm here
+
+		/*Objectif: seperate the url in parts 
+		use the first part to determine the class to load
+		use the second part to determine the method to run
+		while passing all other parts as arguments
+		*/
+
+		$url = self::parseUrl(); //get the url parsed and returned as an array of URL parts
+
+		//use the first part to determine the controller class to load
+
+		if(isset($url[0])){
+		if(file_exists('app/controllers/' . $url[0] . '.php')){
+			$this->controller = $url[0]; //$this refers to the current object
+		}
+		unset($url[0]);
+}
+		$this->controller = 'app\\controllers\\' . $this->controller; //provide a fully qualified classname
+		$this->controller = new $this->controller;
+
+		//use the second part to determine the method to run
+		if(isset($url[1])){
+			if(method_exists($this->controller, $url[1])){
+				$this->method = $url[1];
+			}
+			unset($url[2]);
+		}
+
+		// ... while passing all other parts as argument
+		// repackage the parameters
+
+		$params = $url ? array_values($url) : []; 
+
+		call_user_func_array([$this->controller, $this->method ], $params);
+
+		//var_dump($url); // nice tool to verify w/e data using in application
+
+
+	}
+
+	public static function parseUrl(){
+		if (isset($_GET['url'])) //get url exists
+		{ 							
+			return explode('/', //return parts in an array, separated by /
+				filter_var(	//remove non-URL characters and sequences
+					rtrim($_GET['url'],'/'))
+					,FILTER_SANITIZE_URL);  
+				
+
+		}
 	}
 }
--- app/core/init.php
+++ app/core/init.php
@@ -1,3 +1,8 @@
 <?php
 //echo is useful for debugging
-require_once('app\core\App.php');
+require_once('GitHub\eComA22S2\app\core\App.php');
+
+require("app/core/autoload.php")
+
+//TODO: replace this and other controller definitions by a class autoloader
+// include("app/controllers/Main.php");
